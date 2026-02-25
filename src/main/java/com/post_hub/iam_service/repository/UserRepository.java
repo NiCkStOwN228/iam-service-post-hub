3b378ff1094c441dc0bffe4c478a07e12d748c87
NiCkStOwN228 <nickstown80@icloud.com>
2026-02-25 10:01:10 +0100

    Spring Security + UserDetailsService work with Users

--- src/main/java/com/post_hub/iam_service/repository/UserRepository.java
+++ src/main/java/com/post_hub/iam_service/repository/UserRepository.java
@@ -16,4 +16,8 @@ public interface UserRepository extends JpaRepository<User, Integer>, JpaSpecifi
 
     Optional<User> findByIdAndDeletedFalse(Integer id);
 
+    Optional<User> findUserByEmailAndDeletedFalse(String email);
+
+    Optional<User> findByEmail(String email);
+
 }
