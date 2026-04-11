df41e05859554ca1e797a02e9414138e27b9eaf0
NiCkStOwN228 <nickstown80@icloud.com>
2026-04-11 13:51:09 +0200

    optimising the registration code, moving the validation logic into a separate class

---
+++ src/main/java/com/post_hub/iam_service/security/validation/AccessValidator.java
@@ -0,0 +1,36 @@
+package com.post_hub.iam_service.security.validation;
+
+import com.post_hub.iam_service.model.constants.ApiErrorMessage;
+import com.post_hub.iam_service.model.exception.DataExistException;
+import com.post_hub.iam_service.model.exception.InvalidDataException;
+import com.post_hub.iam_service.repository.UserRepository;
+import com.post_hub.iam_service.utils.PasswordUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AccessValidator {
+
+    private final UserRepository userRepository;
+
+    public void validateNewUser(String username, String email, String password, String confirmPassword) {
+        userRepository.findByUsername(username).ifPresent(existingUser -> {
+            throw new DataExistException(ApiErrorMessage.USERNAME_ALREADY_EXISTS.getMessage(username));
+        });
+
+        userRepository.findByEmail(email).ifPresent(existingUser -> {
+            throw new DataExistException(ApiErrorMessage.EMAIL_ALREADY_EXISTS.getMessage(email));
+        });
+
+
+        if (!password.equals(confirmPassword)) {
+            throw new InvalidDataException(ApiErrorMessage.MISMATCH_PASSWORDS.getMessage());
+        }
+
+        if (PasswordUtils.isNotValidPassword(password)) {
+            throw new InvalidDataException(ApiErrorMessage.INVALID_PASSWORD.getMessage());
+        }
+
+    }
+}
