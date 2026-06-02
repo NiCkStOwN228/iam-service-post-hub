27f3dbd2a7a1d71fa5c5f1dad24ca4a811a565f2
NiCkStOwN228 <nickstown80@icloud.com>
2026-06-02 18:22:26 +0200

    find all comments

--- src/main/java/com/post_hub/iam_service/controller/CommentController.java
+++ src/main/java/com/post_hub/iam_service/controller/CommentController.java
@@ -2,14 +2,19 @@ package com.post_hub.iam_service.controller;
 
 import com.post_hub.iam_service.model.constants.ApiLogMessage;
 import com.post_hub.iam_service.model.dto.comment.CommentDTO;
+import com.post_hub.iam_service.model.dto.comment.CommentSearchDTO;
 import com.post_hub.iam_service.model.request.comment.CommentRequest;
 import com.post_hub.iam_service.model.request.comment.UpdateCommentRequest;
 import com.post_hub.iam_service.model.response.IamResponse;
+import com.post_hub.iam_service.model.response.PaginationResponse;
 import com.post_hub.iam_service.service.CommentService;
 import com.post_hub.iam_service.utils.ApiUtils;
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -57,5 +62,17 @@ public class CommentController {
 
         commentService.softDelete(commentId);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("${end.point.all}")
+    public ResponseEntity<IamResponse<PaginationResponse<CommentSearchDTO>>> getAllComments(
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "limit", defaultValue = "10") int limit) {
+        log.trace(ApiLogMessage.NAME_OF_CURRENT_METHOD.getValue(), ApiUtils.getMethodName());
+
+        Pageable pageable = PageRequest.of(page, limit);
+
+        IamResponse<PaginationResponse<CommentSearchDTO>> response = commentService.findAllComments(pageable);
+        return ResponseEntity.ok(response);
     }
 }
