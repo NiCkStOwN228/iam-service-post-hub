0f22ba7ce168f1b3c3c17e95210d670f2908cd08
NiCkStOwN228 <nickstown80@icloud.com>
2026-06-03 13:39:49 +0200

    search and sort comments

--- src/main/java/com/post_hub/iam_service/model/entity/Comment.java
+++ src/main/java/com/post_hub/iam_service/model/entity/Comment.java
@@ -14,6 +14,11 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class Comment {
 
+    public static final String ID_Field = "id";
+    public static final String MESSAGE_NAME_FIELD = "message";
+    public static final String CREATED_AT_FIELD = "createdBy";
+    public static final String DELETED_AT_FIELD = "deleted";
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
